70fb35a75450cacc556d6afbd67ffe4def2de98a
Blacknight318 <Blacknight318@users.noreply.github.com>
2018-03-23 13:45:37 -0500

    templogger update
    Output formatting added along with comments and csv output.

--- TempLogger.ps1
+++ TempLogger.ps1
@@ -1,5 +1,5 @@
 
-$comList = [System.IO.Ports.SerialPort]::GetPortNames() # Getting list of serial Ports
+$comList = [System.IO.Ports.SerialPort]::GetPortNames() # Getting list of serial Ports with the help of C#
 
 #Space to allow from import of .ini file in event used as a service
 
@@ -7,7 +7,7 @@ $comList = [System.IO.Ports.SerialPort]::GetPortNames() # Getting list of serial
 $com = Read-Host -Prompt "Please enter the serial port name from list $comList" # Need to add COM validation 
 $logLocation = Read-Host -Prompt "Please enter the path where the log will be stored: "
 
-#Setting up the serial port for communication with Arduino
+#Setting up the serial port for communication with Arduino, again using C#
 $port = New-Object System.IO.Ports.SerialPort
 $port.PortName = $com
 $port.BaudRate = "9600"
@@ -22,10 +22,12 @@ while($true){
     $port.Open()
     $temp = $port.ReadLine()
     $port.Close()
+    $temp = $temp -replace "`t|`n|`r","" #Formatting to remove carriage return from serial readline
     $timestamp = Get-Date
     $writeItem = New-Object System.Object
        $writeItem | Add-Member NoteProperty -Name "Temp Farenheit" -Value $temp
        $writeItem | Add-Member NoteProperty -Name "Timestamp" -Value $timestamp
+    $writeItem | Export-Csv -NoTypeInformation -Append -Path $logLocation
     $writeItem
     Start-Sleep -s 5
 }
